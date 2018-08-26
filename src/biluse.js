fb7d408c2587028ee9e72c44fa625a172cc74c98
Rodrigo Clemente <rodrigo.clemente@catho.com>
2018-08-26 19:17:32 -0300

    Inclusão do Bilu na tela

--- src/biluse.js
+++ src/biluse.js
@@ -2,5 +2,15 @@
 
   $('h2').text('Busque Conhecimento!');
 
-
+  $('body').append('<img src="https://uploaddeimagens.com.br/images/001/579/778/full/Bilu.png?1535321592" class="biluse" />');
+  $('head').append(' <style>'+
+  '.biluse {'+
+    'position: fixed;'+
+    'float: left;'+
+    'width: 70px;'+
+    'transform: rotate(45deg);'+
+    'bottom: -15px;'+
+    'left: -15px;'+
+  '}'+
+'</style>');
 })(jQuery);
